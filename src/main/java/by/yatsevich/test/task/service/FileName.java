4d73b126ae6b768648ebaf9b8d9739cd0314f126
tonnarough <yatsevich57@mail.ru>
2022-04-19 00:56:58 +0300

    upd

--- src/main/java/by/yatsevich/test/task/service/FileName.java
+++ src/main/java/by/yatsevich/test/task/service/FileName.java
@@ -1,2 +1,21 @@
-package by.yatsevich.test.task.service;public enum FileName {
+package by.yatsevich.test.task.service;
+
+public enum FileName {
+
+    UNITS("units"),
+    TENS("tens"),
+    HUNDREDS("hundreds"),
+    FROM_TEN_TO_TWENTY("fromTenToTwenty"),
+    POWERS_OF_NUMBER("powersOfNumber");
+
+    private final String fileName;
+
+    FileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
 }
