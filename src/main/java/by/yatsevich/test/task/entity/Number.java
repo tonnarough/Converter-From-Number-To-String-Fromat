5f235a12220245718a51456c17287df92979c0ea
tonnarough <yatsevich57@mail.ru>
2022-04-19 19:39:37 +0300

    added isPositive field, filled fields using private setters

--- src/main/java/by/yatsevich/test/task/entity/Number.java
+++ src/main/java/by/yatsevich/test/task/entity/Number.java
@@ -10,50 +10,91 @@ public class Number {
     private final BigInteger number;
     private int numberOfDigits;
     private int numberOfDegree;
-    private int numberDigitsOfHighestOrder;
+    private int numberDigitsOfHighestDegree;
+    private final boolean isPositive;
     private final List<Integer> digits = new ArrayList<>();
 
     public Number(BigInteger number) {
-        this.number = number;
+
+        if (number.signum() < 0){
+
+            this.number = number.negate();
+            isPositive = false;
+
+        }else{
+
+            this.number = number;
+            isPositive = true;
+
+        }
+
+        setNumberOfDigits();
+        setDigits(this.number);
+        setNumberOfDegree();
+        setNumberDigitsOfHighestDegree();
+
+    }
+
+    public boolean isPositive() {
+        return isPositive;
     }
 
     public List<Integer> getDigits() {
         return digits;
     }
 
-    public void setDigits(int digit) {
-        digits.add(digit);
+    private void setDigits(BigInteger number) {
+
+        for (int i = 0; i < numberOfDigits; i++) {
+
+            digits.add(number.remainder(BigInteger.TEN).intValue());
+            number = number.divide(BigInteger.TEN);
+
+        }
+
     }
 
     public int getNumberOfDigits() {
         return numberOfDigits;
     }
 
-    public void setNumberOfDigits(int numberOfDigits) {
-        this.numberOfDigits = numberOfDigits;
+    private void setNumberOfDigits() {
+
+        this.numberOfDigits = String.valueOf(number).length();
+
     }
 
     public int getNumberOfDegree() {
         return numberOfDegree;
     }
 
-    public void setNumberOfDegree(int numberOfDegree) {
-        this.numberOfDegree = numberOfDegree;
-    }
+    private void setNumberOfDegree() {
 
-    public int getNumberDigitsOfHighestOrder() {
-        return numberDigitsOfHighestOrder;
-    }
+        if (numberOfDigits % 3 == 0) {
 
-    public void setNumberDigitsOfHighestOrder(int numberDigitsOfHighestOrder) {
-
-        if (numberDigitsOfHighestOrder == 0) {
-
-            this.numberDigitsOfHighestOrder = 3;
+            this.numberOfDegree = numberOfDigits / 3;
 
         } else {
 
-            this.numberDigitsOfHighestOrder = numberDigitsOfHighestOrder;
+            this.numberOfDegree = numberOfDigits / 3 + 1;
+
+        }
+
+    }
+
+    public int getNumberDigitsOfHighestDegree() {
+        return numberDigitsOfHighestDegree;
+    }
+
+    private void setNumberDigitsOfHighestDegree() {
+
+        if (getNumberOfDigits() % 3 == 0) {
+
+            this.numberDigitsOfHighestDegree = 3;
+
+        } else {
+
+            this.numberDigitsOfHighestDegree = getNumberOfDigits() % 3;
 
         }
     }
@@ -64,28 +105,32 @@ public class Number {
         if (o == null || getClass() != o.getClass()) return false;
         Number number1 = (Number) o;
         return numberOfDigits == number1.numberOfDigits && numberOfDegree == number1.numberOfDegree
-                && numberDigitsOfHighestOrder == number1.numberDigitsOfHighestOrder
-                && Objects.equals(number, number1.number) && Objects.equals(digits, number1.digits);
+                && numberDigitsOfHighestDegree == number1.numberDigitsOfHighestDegree
+                && isPositive == number1.isPositive && Objects.equals(number, number1.number)
+                && Objects.equals(digits, number1.digits);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(number, numberOfDigits, numberOfDegree, numberDigitsOfHighestOrder, digits);
+        return Objects.hash(number, numberOfDigits, numberOfDegree, numberDigitsOfHighestDegree,
+                isPositive, digits);
     }
 
     @Override
     public String toString() {
+
         StringBuilder numberToString = new StringBuilder();
 
-        numberToString
-                .append("Number{")
+        numberToString.append("Number{")
                 .append("number=").append(number)
                 .append(", numberOfDigits=").append(numberOfDigits)
                 .append(", numberOfDegree=").append(numberOfDegree)
-                .append(", numberDigitsOfHighestOrder=").append(numberDigitsOfHighestOrder)
-                .append(", digits=").append(digits).append("}");
+                .append(", numberDigitsOfHighestDegree=").append(numberDigitsOfHighestDegree)
+                .append(", isPositive=").append(isPositive)
+                .append(", digits=").append(digits).append('}');
 
         return numberToString.toString();
+
     }
 
 }
