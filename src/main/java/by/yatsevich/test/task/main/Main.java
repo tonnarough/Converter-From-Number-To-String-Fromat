09f6e5309cc2603fe9ec0fceb2a92283946298fa
tonnarough <yatsevich57@mail.ru>
2022-04-20 03:57:09 +0300

    upd

--- src/main/java/by/yatsevich/test/task/main/Main.java
+++ src/main/java/by/yatsevich/test/task/main/Main.java
@@ -1,2 +1,14 @@
-package by.yatsevich.test.task.main;public class Main {
+package by.yatsevich.test.task.main;
+
+import by.yatsevich.test.task.service.MathLogic;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(MathLogic.getInstance().getNumberInString(new BigInteger("0")));
+
+    }
+
 }
